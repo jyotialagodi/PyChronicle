4e4d9cea7170a6a65b79461de50ac92536b81499
Jyoti Alagodi <jyotialagodi70@gmail.com>
2026-07-13 16:27:42 +0530

    connect execution timeline button

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 import subprocess
 
-# Function to run tracer.py
+
 def run_program():
     try:
         subprocess.run(["python", "tracer.py"], check=True)
@@ -10,18 +10,24 @@ def run_program():
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
-# Placeholder functions
+
 def timeline():
-    messagebox.showinfo("Timeline", "Timeline Viewer will be connected next.")
+    try:
+        subprocess.run(["python", "timeline_viewer.py"], check=True)
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
 
 def history():
     messagebox.showinfo("History", "Variable History Viewer will be connected next.")
 
+
 def snapshot():
     messagebox.showinfo("Snapshot", "Snapshot Viewer will be connected next.")
 
-# Main Window
+
 root = tk.Tk()
+
 root.title("PyChronicle")
 root.geometry("500x450")
 root.resizable(False, False)
@@ -33,19 +39,24 @@ title = tk.Label(
 )
 title.pack(pady=20)
 
-tk.Button(root, text="Run Program", width=30, height=2,
+tk.Button(root, text="Run Program",
+          width=30, height=2,
           command=run_program).pack(pady=8)
 
-tk.Button(root, text="Execution Timeline", width=30, height=2,
+tk.Button(root, text="Execution Timeline",
+          width=30, height=2,
           command=timeline).pack(pady=8)
 
-tk.Button(root, text="Variable History", width=30, height=2,
+tk.Button(root, text="Variable History",
+          width=30, height=2,
           command=history).pack(pady=8)
 
-tk.Button(root, text="Snapshot Viewer", width=30, height=2,
+tk.Button(root, text="Snapshot Viewer",
+          width=30, height=2,
           command=snapshot).pack(pady=8)
 
-tk.Button(root, text="Exit", width=30, height=2,
+tk.Button(root, text="Exit",
+          width=30, height=2,
           command=root.destroy).pack(pady=20)
 
 root.mainloop()
