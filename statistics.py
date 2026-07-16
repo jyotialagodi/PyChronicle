429e94fc959dd2cb043f1ada38bafb88d0a0b271
Jyoti Alagodi <jyotialagodi70@gmail.com>
2026-07-16 16:44:27 +0530

    Add unique line statistics

--- statistics.py
+++ statistics.py
@@ -2,13 +2,13 @@ import sqlite3
 
 DB_NAME = "pychronicle.db"
 
-# Connect to SQLite database
+# Connect to SQLite Database
 connection = sqlite3.connect(DB_NAME)
 cursor = connection.cursor()
 
-# -----------------------------
+# --------------------------------------
 # Total Records
-# -----------------------------
+# --------------------------------------
 cursor.execute("""
 SELECT COUNT(*)
 FROM variable_states
@@ -16,9 +16,9 @@ FROM variable_states
 
 total_records = cursor.fetchone()[0]
 
-# -----------------------------
+# --------------------------------------
 # Unique Variables
-# -----------------------------
+# --------------------------------------
 cursor.execute("""
 SELECT COUNT(DISTINCT variable_name)
 FROM variable_states
@@ -26,17 +26,28 @@ FROM variable_states
 
 unique_variables = cursor.fetchone()[0]
 
-# -----------------------------
+# --------------------------------------
+# Unique Lines Executed
+# --------------------------------------
+cursor.execute("""
+SELECT COUNT(DISTINCT line_number)
+FROM variable_states
+""")
+
+unique_lines = cursor.fetchone()[0]
+
+# --------------------------------------
 # Display Statistics
-# -----------------------------
-print("=" * 45)
-print("        PYCHRONICLE STATISTICS")
-print("=" * 45)
+# --------------------------------------
 
-print(f"Total Records      : {total_records}")
-print(f"Unique Variables   : {unique_variables}")
+print("=" * 50)
+print("           PYCHRONICLE STATISTICS")
+print("=" * 50)
 
-print("=" * 45)
+print(f"Total Records          : {total_records}")
+print(f"Unique Variables       : {unique_variables}")
+print(f"Unique Lines Executed  : {unique_lines}")
 
-# Close database connection
+print("=" * 50)
+
 connection.close()
